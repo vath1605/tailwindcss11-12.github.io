564f211c8e374b2959f0bb996ca535808551311e
Vath's Macbook Pro <macbookpro@Vaths-MacBook-Pro.local>
2025-02-24 12:17:31 +0700

    nav responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
         myfont : [
           "Sigmar", "serif"
         ]
+      },
+      backgroundImage:{
+        trans:[
+          "linear-gradient(90deg, rgba(0,0,0,0.321187850140056) 0%, rgba(255,255,255,0.5480786064425771) 0%, rgba(0,0,0,0.4008797268907564) 86%)"
+        ],
+        hov:[
+          "linear-gradient(90deg, rgba(159,159,159,0.30438112745098034) 0%, rgba(159,159,159,0.3) 100%)"
+        ]
       }
     },
   },
