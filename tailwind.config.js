9d9e09074dfbc74eca4ff5fb65ca9199da8ae235
Vath's Macbook Pro <macbookpro@Vaths-MacBook-Pro.local>
2025-02-25 12:31:12 +0700

    card

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,29 @@ module.exports = {
         hov:[
           "linear-gradient(90deg, rgba(159,159,159,0.30438112745098034) 0%, rgba(159,159,159,0.3) 100%)"
         ]
-      }
+      },
+      backgroundColor:{
+        dark:[
+          "rgb(40,45,53)"
+        ],
+        ggray:[
+          "rgb(84,93,102)"
+        ]
+      },
+      height:{
+        div:[
+          "635px"
+        ],
+        sec1:[
+          "15%"
+        ],
+        sec2:[
+          "40%"
+        ],
+        sec3:[
+          "45%"
+        ]
+      },
     },
   },
   plugins: [],
